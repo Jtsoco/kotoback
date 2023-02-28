3a0be277b352c430618da65791be97556a7c534f
Vigoluca <lvigotti@gmail.com>
2023-02-28 11:46:05 +0900

    routes made

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+  resources :users do
+    resources :books do
+      resources :cards
+    end
+  end
 end
