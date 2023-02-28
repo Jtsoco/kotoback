0ad31d4f1f5f1767ee038e25c3b677f8a62865bc
kenta asakura <asakura.kenta963420@gmail.com>
2023-02-28 11:17:40 +0900

    first_name, last_name added to users model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def fake_user
+  user = User.new
+  user.first_name
+end
