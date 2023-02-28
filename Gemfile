790e7b8a8d9c63dcd9ede8267cfc07e938fef807
Jackson Socolofsky <jtsoco@gmail.com>
2023-02-28 17:27:44 +0900

    given a folder, can parse the chapters in epub

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "bootsnap", require: false
 
 # Use Sass to process CSS
 gem "sassc-rails"
+# adding Nokogiri
+gem 'nokogiri', '~> 1.14', '>= 1.14.2'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
---
+++ app/services/epub_parser.rb
@@ -0,0 +1,43 @@
+class EpubParser
+  def initialize(folder)
+    @folder = folder
+  end
+
+  def parse_chapters
+   chapters = Dir.glob("app/assets/manuscripts/#{@folder}/**/*.html")
+   chapter_texts = chapters.map do |chapter|
+    Nokogiri::HTML(open(chapter)).text
+   end
+   chapter_texts
+   end
+end
+# require 'nokogiri'
+
+# # doc = Nokogiri::HTML(open('Famous_Modern_Ghost_Stories.html'))
+# # this gets all the id tags and puts them in an array
+# # id_tags = doc.xpath('//@id')
+# # puts id_tags
+# # first = id_tags[3]
+# # puts first
+# # willows = doc.xpath("//a[@id='Willows']/")
+# # puts willows
+
+# # doc2 = Nokogiri::HTML(open('Warbreaker_BrandonSanderson-rev1b-1/index_split_024.html'))
+# # puts doc2.text
+# puts "parsing done"
+# # Dir.foreach('/pg345') do |filename|
+# #   puts filename
+# # end
+
+# # This gives me the path of all html files in the folder beneath
+# # if using irb, press q to escape irb hell
+# list = Dir.glob("pg345/**/*.html")
+# list2 = Dir.glob("Warbreaker_BrandonSanderson-rev1b-1/**/*.html")
+# puts list
+# doc = Nokogiri::HTML(open("Warbreaker_BrandonSanderson-rev1b-1/index_split_040.html")).text
+# # this will give me full text document of the above
+
+# text_arr = list.map do |item|
+#   Nokogiri::HTML(open(item)).text
+# end
+# puts text_arr
