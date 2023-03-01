b5912a5855caed4d03e9fe0b56536c6cffccdee4
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-01 18:12:39 +0900

    cards have chapters attribute now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_021649) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_091018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_021649) do
     t.string "pronunciation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chapter"
+    t.string "chapter_title"
     t.index ["book_id"], name: "index_cards_on_book_id"
   end
 
