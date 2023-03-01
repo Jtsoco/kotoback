5c8045fa623a29b133d60867b9324343ce4ff936
Vigoluca <lvigotti@gmail.com>
2023-03-01 16:03:47 +0900

    api in the right section

--- config/initializers/deepl.rb
+++ config/initializers/deepl.rb
@@ -1,5 +1,5 @@
 DeepL.configure do |config|
-  config.auth_key = "68fc7c46-ce07-1fee-4e73-254bd956df62:fx"
+  config.auth_key = ENV["DEEPL_AUTH_KEY"]
   config.host = "https://api-free.deepl.com"
   config.version = "v2"
 end
