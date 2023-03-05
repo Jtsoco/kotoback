48f1a635bdb0c422bc63b7af84dc2b2e198aa35c
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-05 22:44:22 +0900

    comments added

--- app/services/japanest_to_english_dictionary.rb
+++ app/services/japanest_to_english_dictionary.rb
@@ -2,12 +2,14 @@ class JapaneseToEnglishDictionary
   def initialize
     @dictionary = {}
     Eiwa.parse_file("lib/assets/dictionaries/JMdict_e.xml", type: :jmdict_e) do |entry|
+      # possibility of nil exists, this checks for that
       if !entry.spellings.first.nil?
         japanese = entry.spellings.first.text
       end
       if !entry.meanings.first.definitions.first.nil?
         english =  entry.meanings.first.definitions.first.text
       end
+      # only allow it to be added to hash if both exist
       if english && japanese
         @dictionary[japanese] = english
       end
