7873bf6956e8a1f7438da5e555488d4ede781a93
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-06 15:06:20 +0900

    added complete today

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_091018) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_054915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_091018) do
     t.datetime "updated_at", null: false
     t.integer "chapter"
     t.string "chapter_title"
+    t.boolean "completed_today"
     t.index ["book_id"], name: "index_cards_on_book_id"
   end
 
