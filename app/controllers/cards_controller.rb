dedf709a9cce11acb35f9824e5c6513b3b31b579
Vigoluca <lvigotti@gmail.com>
2023-03-06 13:10:59 +0900

    edit page link added

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,21 @@
 class CardsController < ApplicationController
   # before_action :set_book
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.user = current_user
+    @cards = current_user.cards
+    authorize @card
+    if @card.save
+      redirect_to :book
+    else
+      @tab = "new-tab"
+      render "new_card", status: :unprocessable_entity
+    end
+  end
 
   def edit
     # @book = Book.find(params[:book_id])
