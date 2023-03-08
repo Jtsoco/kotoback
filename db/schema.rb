b6b4e16f824fb68dc25446cb2a7286f81704d648
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-08 16:41:31 +0900

    fixed it again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_073013) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_074102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,7 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_073013) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.integer "chapters"
     t.bigint "user_id", null: false
     t.string "genre"
     t.string "image_url"
