a94b935c3752e6db2b18d82def380e04e7c18f54
kenta asakura <asakura.kenta963420@gmail.com>
2023-03-09 20:54:14 +0900

    fetch book service updated

--- app/services/fetch_book_cover.rb
+++ app/services/fetch_book_cover.rb
@@ -7,14 +7,13 @@ class FetchBookCover
     url = "https://openlibrary.org/search.json?q=#{@book.title}"
     book_serialized = URI.open(url).read
     book_url = JSON.parse(book_serialized)
-    book_isbn = book_url["docs"].first["isbn"][0]
 
-    url = "https://openlibrary.org/api/books?bibkeys=ISBN:#{book_isbn}&format=json&jscmd=data"
-    book_serialized = URI.open(url).read
-    json_book = JSON.parse(book_serialized)
-    book_cover = json_book["ISBN:#{book_isbn}"]
-
-    if book_cover.include?("cover")
+    if book_url["docs"].first.include?('isbn')
+      book_isbn = book_url["docs"].first["isbn"][0]
+      url = "https://openlibrary.org/api/books?bibkeys=ISBN:#{book_isbn}&format=json&jscmd=data"
+      book_serialized = URI.open(url).read
+      json_book = JSON.parse(book_serialized)
+      book_cover = json_book["ISBN:#{book_isbn}"]
       @book.image_url = json_book["ISBN:#{book_isbn}"]["cover"]["medium"]
     else
       @book.image_url = "https://howtodrawforkids.com/wp-content/uploads/2022/07/how-to-draw-an-open-book.jpg"
