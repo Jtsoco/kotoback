27afd10ef0c3d7249d83696333d9517fcba66d07
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-09 12:57:30 +0900

    better tokenizer, gets minor filtering done

--- app/services/text_tokenizer_japanese.rb
+++ app/services/text_tokenizer_japanese.rb
@@ -8,6 +8,7 @@ class TextTokenizerJapanese
     # ( ) parenthesis must still be removed
     # take care, as it's the japanese keyboard parentheses, not western keyboard default
     @japanese_regex = /[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]/
+    @punctuation_regex = /[（）？！]/
   end
   def tokenize_all
     tokenized_arr = @array.map do |text|
@@ -17,11 +18,11 @@ class TextTokenizerJapanese
   end
 
   def tokenize(text)
-    tokenized = tagger.parse(text).map {|token| token}
+    tokenized = @tagger.parse(text).map {|token| token}
     tokenized_filtered = tokenized.map {|word| word.split(/[\s,]/)}
-    japanese_only = tokenized_filtered.select{|arr| @regex.match?(arr[0])}
+    japanese_only = tokenized_filtered.select{|arr| @japanese_regex.match?(arr[0])}
     # reject special parenthesis
-    japanese_only_filtered = japanese_only.reject{|arr| arr[0].include?("（") || arr[0].include?("）")}
+    japanese_only_filtered = japanese_only.reject{|arr| @punctuation_regex.match?(arr[0])}
     japanese_only_filtered
   end
 end
