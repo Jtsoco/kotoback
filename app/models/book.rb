2d2a394e596e08e4d06dbbd9de63480845a796df
Jackson Socolofsky <jtsoco@gmail.com>
2023-03-16 15:39:16 +0900

    added simple rspec tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,5 +8,12 @@ class Book < ApplicationRecord
     en_ja: 0,
     ja_en: 1
   }
-
+  def how_many_chapters
+    if self.cards == []
+      'No chapter data'
+    else
+      chapter_count = self.cards.pluck(:chapter).uniq.max
+      chapter_count == 1 ? "There is 1 chapter" : "There are #{chapter_count} chapters"
+    end
+  end
 end
